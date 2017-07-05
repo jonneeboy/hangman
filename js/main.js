3c4b0c000e1ca70ea821ef4e334c959f93da21bf
jonathan lee <leejonathan66@gmail.com>
2017-07-05 16:01:34 -0700

    update hangman

---
+++ js/main.js
@@ -0,0 +1,114 @@
+
+/*----- constants -----*/
+// source of secret words
+var words = [
+    'PAR', 
+    'BIRDIE',
+    'BOGIE',
+    'EAGLE',
+    'DRIVER',
+    'ALCATRAZ',
+    'CADDIE',
+    'PUTT'
+];
+
+
+/*----- app's state (variables) -----*/
+var lives 
+var used
+var wrongCount
+var secretWord
+var guess
+var letterBoard
+var winner
+var reset
+var hint
+var resetbtn = document.querySelector('#reset');
+var start = document.querySelector('start');
+// var alphabet = ABCDEFGHIJKLMNOPQRSTUVWXYZ;
+/*----- cached element references -----*/
+var $guess = $('#guess');
+var $img = $('#hang-img');
+var $player = $('#player');
+var $countLives = document.getElementById("lives");
+var $message = $('#message');
+/*----- event listeners -----*/
+
+$('table').on('click', 'td', handleLetterClick);
+
+$('#reset').on('click', resetGame);
+$('#wrong').on('click', wrongCount);
+/*----- functions -----*/
+
+resetGame();
+
+function resetGame() {
+    wrongCount = 6;
+    winner = 0;
+    secretWord = words[getRandomInt(words.length - 1)];
+    guess = '_'. repeat(secretWord.length);
+    used = [];
+    $('td').removeClass('disable-td');
+    render();
+}
+
+function handleLetterClick(evt) {
+    if (wrongCount === 0) return;
+
+    console.log(secretWord);
+    var letter = evt.target.textContent;
+    if (used.includes(letter)) {
+        return;
+    } else {
+        used.push(letter);
+    }
+    if (secretWord.includes(letter)) {
+        var pos = secretWord.indexOf(letter);
+        while (pos >= 0){
+            guess = guess.split('');
+            guess[pos] = letter;
+            guess = guess.join('');
+            pos = secretWord.indexOf(letter, pos + 1);
+        }
+    } else if (wrongCount > 0) {
+        wrongCount--;
+    }
+    render(); 
+}   
+    
+
+  
+ function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function render() {
+    $img.attr('src', 'images/img' + wrongCount + '.png');
+
+    $guess.html(guess);
+    $('#wrong').html(wrongCount);
+    used.forEach(function(letter) {
+        $('#'+letter).addClass('disable-td');
+    });
+    if (guess === secretWord) {
+        $message.html("HOLE IN ONE!!"); 
+    } else if (wrongCount === 0) {
+        $message.html("Mulligan??");
+    } else {
+        $message.html("");
+    }
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
---
+++ psuedocode.md
@@ -0,0 +1,43 @@
+
+
+HANGMAN golf
+
+draw a man hanging by a when lost return a face, boby oak tree by block tables or divs.   
+
+player 1 vs. computer. 
+    or if possible player vs player
+
+
+computer will generate a word by random. 
+    the player will need to compute each letter, til he or she gets it right by returning a correct letter.
+
+    when player calls letter wrong when lost, return a face, body, legs, arms. 
+
+    if player calls wrong letter ten times, then return a game over.
+
+    if the cell letter is right then return a pass and player returns moves on.  
+
+    if the player guesses all letters right and gets the correct word then always return a win.
+
+    add a  big alert sign: player wins will show up.
+
+when button is clicked return a letter called by the alphabet, 
+    return a letter, when letter is wrong compute a blocked out cell color grey. 
+  
+
+array of golf words set to be picked randomly returning a length of words.
+    add a array of catagories
+
+hint button by onclick...
+    address golf hints and return.
+
+ call a replay button at the top of the game,       when clicked return a new game.
+
+reasign words that answer the question related      to golf by random, return x amt of empty        lines.
+
+
+
+ 
+
+
+
